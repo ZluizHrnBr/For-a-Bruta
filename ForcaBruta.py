bdc0c50a43dd9cbdb39c61a8fd69931a3e2f921b
ZluizHrnBr <82054071+ZluizHrnBr@users.noreply.github.com>
2025-02-16 22:43:50 -0300

    terceiro commit

--- ForcaBruta.py
+++ ForcaBruta.py
@@ -1,30 +1,13 @@
+from itertools import combinations
 import pandas as pd
 import time
 
+alunos_habilidades = pd.read_csv("alunos_habilidades.csv")
+Lista_alunos_habilidades = alunos_habilidades.to_dict(orient="records")
 
 
-# Definir parâmetros
 
-
-alunos = pd.read_csv('matriz_habilidades.csv')
-alunos_dict = alunos.to_dict(orient='records')
-tamanho_equipe = 5
-def combinar(lista, k):
-  
-    if k == 0:
-        return [[]]
-    if not lista:
-        return []
-    return combinar(lista[1:], k) + [[lista[0]] + resto for resto in combinar(lista[1:], k - 1)]
-
-# Gerar todas as combinações de equipes
-inicio = time.time()
-
-melhor_equipe = None
-melhor_fitness = 0
-total_combinacoes = 0
-
-habilidades_prioridade = {
+habilidades_prioridade_projeto = {
     3: ['DW', 'DM', 'DB'],  # Alta prioridade
     2: ['AS', 'TS'],  # Média prioridade
     1: ['DF']  # Baixa prioridade
@@ -36,28 +19,34 @@ pesos_prioridade = {
     1: 1   # Baixa prioridade tem peso 1
 }
 
-combinacoes = combinar(alunos_dict, tamanho_equipe)
 
-for equipe in combinacoes:
+tamanho_equipe_estudantes = 5
+equipes_estudantes_habilidades = list(combinations(Lista_alunos_habilidades, tamanho_equipe_estudantes))
+melhor_equipe_projeto = None
+melhor_fitness = 0
+total_combinacoes = 0
+
+inicio = time.time()
+
+for equipes_estudantes in equipes_estudantes_habilidades:
     total_combinacoes += 1
     fitness = 0
-    habilidades = set()
-    
+    habilidades_equipe_necessarias = set()
 
-    for aluno in equipe:
-        for habilidade in habilidades_prioridade[3] + habilidades_prioridade[2] + habilidades_prioridade[1]:
-            if aluno[habilidade] > 0:  
-                habilidades.add(habilidade)
-   
-    for prioridade, habilidades_prioritarias in habilidades_prioridade.items():
-        for habilidade in habilidades_prioritarias:
-            if habilidade in habilidades:
-                fitness += pesos_prioridade[prioridade]
-    
+    for equipes_alunos in equipes_estudantes:
+        #Iterando sobre as chaves do dicionário das habilidades por prioridades para os projetos de software.
+        for habilidades_prioridades in habilidades_prioridade_projeto[3] + habilidades_prioridade_projeto[2] + habilidades_prioridade_projeto[1]:
+            if equipes_alunos[habilidades_prioridades] > 0:
+                habilidades_equipe_necessarias.add(habilidades_prioridades)
+
+    for prioridade, habilidades_prioritarias in habilidades_prioridade_projeto.items():
+        for habilidades_essenciais in habilidades_prioritarias:
+            if habilidades_essenciais in habilidades_equipe_necessarias:
+               fitness +=  pesos_prioridade[prioridade] 
+
     if fitness > melhor_fitness:
         melhor_fitness = fitness
-        melhor_equipe = equipe
-
+        melhor_equipe_projeto = equipes_estudantes
 
 fim = time.time()
 tempo_total = fim - inicio
@@ -67,9 +56,10 @@ segundos = tempo_total % 60
 horas = minutos / 60
 
 # Resultados
-print(f"Tempo total: {horas}:{minutos}:{segundos}")
+print(f"Tempo total: {minutos}:{segundos:.1f}")
 print(f"Melhor fitness: {melhor_fitness}")
-print(f"total de combinações realizadas: {total_combinacoes}")
+print(f"total de combinações realizadas:{total_combinacoes}")
 print("\nMelhor equipe:")
-for membro in melhor_equipe:
-    print(membro)
+
+for equipe_alunos_habilidades in melhor_equipe_projeto:
+    print(equipe_alunos_habilidades)
